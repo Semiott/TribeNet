975eb84d38ca7b24a614bad0f389041c22188bbe
Gokul Alex <gokulsan@live.com>
2020-09-30 21:25:41 +0530

    Changes to be committed: Resolved Issues with Matic Network Setup

--- smartcontracts/truffle-config.js
+++ smartcontracts/truffle-config.js
@@ -1,6 +1,4 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider')
-const fs = require('fs');
-const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   networks: {
